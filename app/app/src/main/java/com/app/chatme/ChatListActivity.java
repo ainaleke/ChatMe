d47ac3a3a99cc38d10672c9a20445a79b31750d5
Oluwaleke Aina <ainaleke@gmail.com>
2016-01-23 12:33:27 -0500

    Build the List of friends for the Logged in user

--- app/app/src/main/java/com/app/chatme/ChatListActivity.java
+++ app/app/src/main/java/com/app/chatme/ChatListActivity.java
@@ -24,7 +24,7 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
 public class ChatListActivity extends AppCompatActivity {
     Button logout;
