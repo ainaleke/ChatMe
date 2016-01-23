54b190a3d2d5bb6cb1de3a4d321eb95068a5eb43
Oluwaleke Aina <ainaleke@gmail.com>
2016-01-23 12:35:21 -0500

    Adapter for ChatActivity.java

--- app/app/src/main/java/com/app/chatme/ChatScreenAdapter.java
+++ app/app/src/main/java/com/app/chatme/ChatScreenAdapter.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.TextView; 
 
 import com.parse.ParseObject;
 import com.parse.ParseUser;
